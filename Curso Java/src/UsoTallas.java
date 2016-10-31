34052663d266d95cb174c4e394c081bfa64de5a9
sergio <sergio@eeepc>
2016-10-31 17:33:44 -0400

    49 Actualizado...

---
+++ Curso Java/src/UsoTallas.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class UsoTallas {
+
+	//enum Talla {MINI,MEDIANO,GRANDE,MUY_GRANDE};
+	
+	enum Talla {
+		
+		MINI("S"), MEDIANO("M"),GRANDE("L"),MUY_GRANDE("XL");
+		
+		private Talla(String abreviatura){
+			
+			this.abreviatura=abreviatura;
+			
+		}
+		
+		public String dameAbreviatura(){
+			
+			return abreviatura;
+		}
+		
+		private String abreviatura;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Talla s=Talla.MINI;
+		Talla m=Talla.MEDIANO;
+		Talla l=Talla.GRANDE;
+		Talla xl=Talla.MUY_GRANDE;*/
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Escribe una talla: MINI, MEDIANO, GRANDE, MUY_GRANDE");
+		
+		String entradaDatos=entrada.next().toUpperCase();
+		
+		Talla laTalla=Enum.valueOf(Talla.class, entradaDatos);
+		
+		System.out.println("Talla: "+laTalla);
+		
+		System.out.println("Abreviatura: "+laTalla.dameAbreviatura());
+
+	}
+
+}
